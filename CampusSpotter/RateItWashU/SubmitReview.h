4229edb370dab520fe993495fc0896868e2c126e
Parker Crist <parkercrist@go.wustl.edu>
2014-04-15 18:05:32 -0500

    Almost finished detail views

---
+++ CampusSpotter/RateItWashU/SubmitReview.h
@@ -0,0 +1,13 @@
+//
+//  SubmitReview.h
+//  RateItWashU
+//
+//  Created by Parker Crist on 4/15/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SubmitReview : UIViewController
+
+@end
---
+++ CampusSpotter/RateItWashU/SubmitReview.m
@@ -0,0 +1,38 @@
+//
+//  SubmitReview.m
+//  RateItWashU
+//
+//  Created by Parker Crist on 4/15/14.
+//
+//
+
+#import "SubmitReview.h"
+
+@interface SubmitReview ()
+
+@end
+
+@implementation SubmitReview
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
